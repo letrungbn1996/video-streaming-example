bcd5db0f52cf08168006bdea53aa8a423b831ce8
le trung <letrungbn1996@gmail.com>
2021-11-15 12:06:39 +0700

    video streaming

---
+++ server/app.js
@@ -0,0 +1,79 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const cors = require('cors');
+const thumbsupply = require('thumbsupply');
+const app = express();
+app.use(cors());
+const videos = [
+    {
+        id: 0,
+        poster: 'https://images.ctfassets.net/hrltx12pl8hq/7yQR5uJhwEkRfjwMFJ7bUK/dc52a0913e8ff8b5c276177890eb0129/offset_comp_772626-opt.jpg?fit=fill&w=800&h=300',
+        duration: '3 mins',
+        name: 'Sample 1'
+    },
+    {
+        id: 1,
+        poster: 'https://images.ctfassets.net/hrltx12pl8hq/7yQR5uJhwEkRfjwMFJ7bUK/dc52a0913e8ff8b5c276177890eb0129/offset_comp_772626-opt.jpg?fit=fill&w=800&h=300',
+        duration: '4 mins',
+        name: 'Sample 2'
+    },
+    {
+        id: 2,
+        poster: 'https://images.ctfassets.net/hrltx12pl8hq/7yQR5uJhwEkRfjwMFJ7bUK/dc52a0913e8ff8b5c276177890eb0129/offset_comp_772626-opt.jpg?fit=fill&w=800&h=300',
+        duration: '2 mins',
+        name: 'Sample 3'
+    },
+];
+
+app.get('/videos', (req, res) => {
+    res.json(videos);
+})
+
+app.get('/video/:id/poster', (req, res) => {
+    thumbsupply.generateThumbnail(`assets/${req.params.id}.mp4`)
+    .then(thumb => res.sendFile(thumb));
+});
+
+app.get('/video/:id/data', (req, res) => {
+    const id = parseInt(req.params.id, 10);
+    res.json(videos[id]);
+});
+
+app.get('/video/:id/caption', (req, res) => res.sendFile('assets/captions/sample.vtt', { root: __dirname }));
+
+
+app.get('/video/:id', (req, res) => {
+    const path = `assets/${req.params.id}.mp4`;
+    const stat = fs.statSync(path);
+    const fileSize = stat.size;
+    const range = req.headers.range;
+    if (range) {
+        const parts = range.replace(/bytes=/, "").split("-");
+        const start = parseInt(parts[0], 10);
+        const end = parts[1]
+            ? parseInt(parts[1], 10)
+            : fileSize-1;
+        const chunksize = (end-start) + 1;
+        const file = fs.createReadStream(path, {start, end});
+        const head = {
+            'Content-Range': `bytes ${start}-${end}/${fileSize}`,
+            'Accept-Ranges': 'bytes',
+            'Content-Length': chunksize,
+            'Content-Type': 'video/mp4',
+        };
+        res.writeHead(206, head);
+        file.pipe(res);
+    } else {
+        const head = {
+            'Content-Length': fileSize,
+            'Content-Type': 'video/mp4',
+        };
+        res.writeHead(200, head);
+        fs.createReadStream(path).pipe(res);
+    }
+});
+
+app.listen(4000, () => {
+    console.log('Listening on port 4000!')
+});
